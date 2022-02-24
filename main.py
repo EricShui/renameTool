efd18696128e7c4afb7326d17d1bf077c6cd5b07
EricShui <2426726839@qq.com>
2022-02-24 22:25:22 +0800

    add read file , button function , multithread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
+import sys, os
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QThread, Qt
 from ui_mainwindow import Ui_MainWindow
+from time import sleep
 
 
 class Rename_Tool(QMainWindow):
@@ -10,6 +12,61 @@ class Rename_Tool(QMainWindow):
         self.ui.setupUi(self)  # 构造UI界面
         self.setWindowTitle("批量重命名")  # 窗口标题
         self.setCentralWidget(self.ui.centralwidget)
+        self.ui_widgets = [self.ui.prefi_text, self.ui.preconnector_text, self.ui.name_text,
+                           self.ui.suffix_connecto_text, self.ui.suffix_text, self.ui.file_suffix_text]
+
+    def rename_func(self, list, pattern):
+        pass
+
+    def process_file_list(self, list):
+        self.rename_file = WorkThread(self.rename_func, '')
+        self.rename_file.start()
+        self.rename_file.trigger.connect(lambda x: x)
+
+    @pyqtSlot(bool)
+    def on_start_button_clicked(self):
+        self.read_file = WorkThread(self.file_list, './')
+        self.read_file.start()
+        self.read_file.trigger.connect(lambda x: self.process_file_list(x))
+
+        # self.ui.progressBar.setTextVisible(True)
+        # for x in range(0, 101, 100 // 33):
+        #     if x >= 98:
+        #         self.ui.progressBar.setValue(100)
+        #     else:
+        #         self.ui.progressBar.setValue(x)
+        #     sleep(1)
+
+        # if not state:
+        #     for widgets in self.ui_widgets:
+        #         widgets.setDisabled(True)
+
+    def file_list(self, path):
+        file_list = os.listdir(path)
+        res = []
+        for each in file_list:
+            temp = each.split('.')
+            if len(temp) < 2:
+                file_list.remove(each)
+            elif temp[0] == '':
+                file_list.remove(each)
+            else:
+                res.append(each)
+        return res
+
+
+class WorkThread(QThread):
+    trigger = pyqtSignal(list)
+
+    def __init__(self, fun, par):
+        super(WorkThread, self).__init__()
+        self.par = par
+        self.fun = fun
+
+    def run(self):
+        res = self.fun(self.par)
+        # print(res)
+        self.trigger.emit(res)
 
 
 def main():
@@ -22,4 +79,3 @@ def main():
 
 if __name__ == "__main__":  # 用于当前窗体测试
     main()
-
